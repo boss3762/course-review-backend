b8432526c272fda3417137bdcb8569bb89f279cb
Phattarapol <wpphattarapol@gmail.com>
2026-02-16 14:50:10 +0700

    feat: Update `findAll` endpoint to return a list of course objects.

--- src/courses.controller.ts
+++ src/courses.controller.ts
@@ -4,8 +4,10 @@ import { Controller, Get } from '@nestjs/common';
 export class CoursesController {
   @Get()
   findAll(): any {
-    return {
-      message: 'Good Morning',
-    };
+    return [
+      {id: '100',number: '01204111', title: 'Computer and Programming'},
+      {id: '101',number: '01204112', title: 'React'},
+      {id: '102',number: '01204113', title: 'Node.js'},
+    ];
   }
 }

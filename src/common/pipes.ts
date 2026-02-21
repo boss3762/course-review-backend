ced66b1494ddc6331bebb98d0098dca050198927
Phattarapol <wpphattarapol@gmail.com>
2026-02-21 12:04:35 +0700

    chore: Create `pipes.ts` with a commented `ParseObjectIdPipe` implementation.

---
+++ src/common/pipes.ts
@@ -0,0 +1,15 @@
+// import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common';
+// import { ObjectId } from 'mongodb';
+
+// @Injectable()
+// export class ParseObjectIdPipe implements PipeTransform<any,ObjectId> {
+    
+//     public transform(value: any): ObjectId {
+//         try{
+//             const transformedObjectId: ObjectId = ObjectId.createFromHexString(value);
+//             return transformedObjectId;
+//         } catch (error) {
+//             throw new BadRequestException('Invalid Object ID');
+//         }
+//     }
+// }
